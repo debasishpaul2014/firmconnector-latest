2dab3208024ecb36006df0ff6de63a2e2f24664a
Debasish Paul <debasishpaul2014@gmail.com>
2021-08-04 22:28:34 +0530

    UI changes

--- src/components/HomeComponents/ThirdBlock.js
+++ src/components/HomeComponents/ThirdBlock.js
@@ -5,25 +5,33 @@ import ButtonLg from "../Buttons/ButtonLg";
 
 const ThirdBlock = () => {
   return (
-    <div className="bg-dark-custom text-white">
-      <div className="container col-xxl-8 px-4 py-5">
-        <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-          <div className="col-10 col-sm-8 col-lg-6">
-            <img src={tell_company} className="d-block mx-lg-auto img-fluid" />
+    <div className="bg-dark-custom text-white third-block">
+      <div className="container">
+        <div className="row flex-lg-row-reverse align-items-center py-5">
+          <div className="col-12 col-sm-8 col-lg-5 p-4">
+            <img
+              src={tell_company}
+              className="d-block mx-lg-auto img-fluid"
+              alt=""
+            />
           </div>
-          <div className="col-lg-6">
-            <h1 className="display-6 mb-3">Gain easy access to specialists</h1>
-            <p>
+          <div className="col-lg-7">
+            <h1 className="display-6 mb-3 fw-bold">
+              Gain easy access to specialists
+            </h1>
+            <p className="text-muted-custom">
               Staffing needs can change quickly when a new project lands or a
               position needs to be filled. Perhaps you’re already working on a
               project, and need to identify a specialist to bounce ideas off of.
             </p>
-            <p>
+            <p className="text-muted-custom">
               FirmConnector allows you to quickly and easily access the right
               people with the right skills for the job.
             </p>
-            <h1 className="display-6 mb-3 text-warning">Fast. Easy. And…</h1>
-            <p>
+            <h1 className="display-6 mb-3 fw-bold text-success-custom">
+              Fast. Easy. And…
+            </h1>
+            <p className="text-muted-custom">
               Best of all, you will ensure you are fully utilizing your own
               workforce before turning to outside sources.
             </p>
--- src/components/HomeComponents/TopBanner.js
+++ src/components/HomeComponents/TopBanner.js
@@ -1,34 +1,40 @@
 import React from "react";
 
 import banner_image from "../../assets/images/banner_left.svg";
+import home_banner_bd from "../../assets/images/home_banner_bd.svg";
 import ButtonLg from "../Buttons/ButtonLg";
+import "./home.css";
 
 const TopBannerComponent = () => {
   return (
-    <div className="jumbotron bg-info bg-gradient">
-      <div className="container pt-5 pb-5">
+    <div className="jumbotron bg-black-custom top-banner py-5">
+      <div className="container">
         <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-6">
-            <h1 className="ddisplay-6 text-white">
-              Maximize the value of your network
-            </h1>
-            <p className=" text-dark">
-              FirmConnector is software as a service that can provide your
-              organization with quick, easy access to the skillsets and
-              availability of your entire talent network both your in-house
-              workforce and through trusted vendors and partners.
-            </p>
-            <p>
+          <div className="col-sm-12 col-md-7 col-lg-7">
+            <div className="d-block mb-4">
+              <h1 className="display-4 text-white fw-bold">
+                Maximize the value of your network
+              </h1>
+            </div>
+            <div className="d-block mb-4">
+              <p className="lead text-muted-custom mb-4">
+                FirmConnector is software as a service that can provide your
+                organization with quick, easy access to the skillsets and
+                availability of your entire talent network both your in-house
+                workforce and through trusted vendors and partners.
+              </p>
+            </div>
+            <div className="d-block">
               <ButtonLg
-                className="btn-warning-custom"
+                className="btn-success-custom"
                 role="button"
                 title="Click here to learn more"
                 type="button"
                 to="sign-in"
               />
-            </p>
+            </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-lg-6 d-flex justify-content-lg-end d-none d-md-block d-lg-block d-xl-block d-xxl-block">
+          <div className="col-sm-12 col-md-5 col-lg-5 d-flex justify-content-lg-end d-none d-md-block d-lg-block d-xl-block d-xxl-block">
             <img src={banner_image} className="img-fluid" alt="..." />
           </div>
         </div>
