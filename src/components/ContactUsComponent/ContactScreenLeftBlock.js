4fe20c1cff87be08044f79285902b241dda47f33
Debasish Paul <debasishpaul2014@gmail.com>
2021-08-06 00:48:25 +0530

    UI changes

--- src/components/ContactUsComponent/ContactScreenLeftBlock.js
+++ src/components/ContactUsComponent/ContactScreenLeftBlock.js
@@ -1,18 +1,15 @@
 import React from "react";
 import contact_us from "../../assets/images/contact_us.svg";
-import HeaderSm from "../Headers/HeaderSm";
 
 const ContactScreenLeftBlock = () => {
   return (
     <div className="col-12 col-sm-12 col-md-12 col-lg-5">
       <div className="d-block mb-4">
         <div className="d-block">
-          <span className="h5 fw-bold-custom">Support email</span>
+          <span>Please get in touch. We'd love to hear from you.</span>
         </div>
         <div className="d-block">
-          <span className="text-info-light-custom">
-            support@firmconnector.com
-          </span>
+          <span className="text-info-light-custom">info@firmconnector.com</span>
         </div>
       </div>
       <div className="d-block login-page-lg-image mb-4">
