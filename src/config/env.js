f6c80739f02670e4ae606b5df807e6cb8c20ed89
Debasish Paul <debasishpaul2014@gmail.com>
2022-03-12 23:07:04 +0530

    UI changes

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,8 @@
-const API_BASE =
-  "https://www.firmconnector.com/firmconnector-api-new/index.php/";
+const API_BASE = "http://44.237.193.208/index.php/";
 
-const ASSETS_BASE =
-  "https://www.firmconnector.com/firmconnector-api-new/assets/";
-const PROFILE_IMAGE_BASE =
-  "https://www.firmconnector.com/firmconnector-api-new/";
+const ASSETS_BASE = "http://44.237.193.208/assets/";
+const PROFILE_IMAGE_BASE = "http://44.237.193.208/";
 
-const FIRM_IMAGE_BASE = "https://www.firmconnector.com/firmconnector-api-new/";
+const FIRM_IMAGE_BASE = "http://44.237.193.208/";
 
 export { API_BASE, ASSETS_BASE, PROFILE_IMAGE_BASE, FIRM_IMAGE_BASE };
