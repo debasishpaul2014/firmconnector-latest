f0609a4bfd84af02fdc0c9c07ba8d6544064090f
Debasish Paul <debasishpaul2014@gmail.com>
2022-03-31 23:13:42 +0530

    profile page issue solved

--- src/components/Resources/ResourceProfile/ResourceProfileTopSection.js
+++ src/components/Resources/ResourceProfile/ResourceProfileTopSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import ProfileBrief from "./ProfileBrief";
 import GeneralInfo from "./GeneralInfo";
@@ -9,23 +9,29 @@ import "./profile.css";
 const ResourceProfileTopSection = (props) => {
   const { displayView, resourceDetails } = props;
 
+  useEffect(() => {}, [resourceDetails]);
+
   return (
     <div className="row">
-      <ProfileBrief
-        displayView={displayView}
-        profileDetails={resourceDetails.profile_details}
-        contactDetails={resourceDetails.contact_details}
-      />
-      <GeneralInfo
-        displayView={displayView}
-        resourceDetails={resourceDetails.profile_details}
-        contactDetails={resourceDetails.contact_details}
-      />
-      <SkillSet
-        skillDetails={resourceDetails.skill_details}
-        skillCount={resourceDetails.skill_count}
-        skillFormattedSet={resourceDetails.skill_formatted_set}
-      />
+      {resourceDetails ? (
+        <>
+          <ProfileBrief
+            displayView={displayView}
+            profileDetails={resourceDetails.profile_details}
+            contactDetails={resourceDetails.contact_details}
+          />
+          <GeneralInfo
+            displayView={displayView}
+            resourceDetails={resourceDetails.profile_details}
+            contactDetails={resourceDetails.contact_details}
+          />
+          <SkillSet
+            skillDetails={resourceDetails.skill_details}
+            skillCount={resourceDetails.skill_count}
+            skillFormattedSet={resourceDetails.skill_formatted_set}
+          />
+        </>
+      ) : null}
     </div>
   );
 };
